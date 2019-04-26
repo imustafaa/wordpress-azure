e3a3106b0bfe1a4ca28106331736451f713c238b
imustafaa <imustafaa@incapitalmg.com>
2019-04-26 17:28:05 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -121,4 +121,5 @@ if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once(ABSPATH . 'wp-settings.php');define( 'WP_ALLOW_REPAIR', true );
+
